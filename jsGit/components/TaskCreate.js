77f0650cddeab803c70a7561c3a97ba554934fdd
Artas228 <75374987+Artas228@users.noreply.github.com>
2020-12-28 19:09:10 +0600

    Add files via upload

---
+++ jsGit/components/TaskCreate.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+export default class TaskCreate extends Component {
+    render() {
+    return (
+    <>
+        <input />
+        <button>Add </button>
+    </>
+    );
+    }
+}
---
+++ jsGit/components/TaskList/TaskLIst.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+export default class TasksList extends Component {
+  render() {
+    return (
+      <>
+        <div>task 1</div>
+        <div>task 2</div>
+        <div>task 3</div>
+      </>
+    );
+  }
+}
